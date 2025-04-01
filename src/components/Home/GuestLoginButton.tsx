0b3231b1ce1622a8c7ace3751e44c6705c38a9bf
oxamyt <dentez.danil@gmail.com>
2025-04-01 21:10:24 +0300

    fix(GuestLoginButton): add loading state

--- src/components/Home/GuestLoginButton.tsx
+++ src/components/Home/GuestLoginButton.tsx
@@ -3,11 +3,12 @@ import { useAppDispatch } from "@/store/hooks";
 import { setUser } from "@/store/authSlice";
 import { useNavigate } from "react-router-dom";
 import { FaPerson } from "react-icons/fa6";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export function GuestLoginButton() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const [guestLogin] = useGuestLoginMutation();
+  const [guestLogin, { isLoading }] = useGuestLoginMutation();
 
   async function onLogin() {
     try {
@@ -31,7 +32,18 @@ export function GuestLoginButton() {
       onClick={onLogin}
       className="bg-gradient-to-r flex justify-center items-center from-orange-400 via-pink-500 to-purple-500 mt-2 text-white font-bold text-lg py-4 px-4 rounded-lg text-center w-64"
     >
-      <FaPerson className="w-7 h-7" /> Guest User
+      {isLoading ? (
+        <>
+          {" "}
+          <FaPerson className="w-7 h-7" /> Guest User{" "}
+          <AiOutlineLoading3Quarters className="animate-spin ml-1" />
+        </>
+      ) : (
+        <>
+          {" "}
+          <FaPerson className="w-7 h-7" /> Guest User
+        </>
+      )}
     </button>
   );
 }
